4bff3459e7e9c6e52b3d728151e50a4a3f6105da
ravina1606 <160000675+ravina1606@users.noreply.github.com>
2025-03-16 23:57:01 +0530

    Add files via upload

---
+++ pracrice_set_12.java
@@ -0,0 +1,28 @@
+package calc;
+
+class calculator{
+    public void  calculate(int a,int b){
+        System.out.println("your result is:" + a+b);
+    }
+
+}
+class Sccalculator{
+    public void  calculate(int a,int b){
+        System.out.println("your result is:" + Math.sin(a+b));
+    }
+
+}
+class Hycalculator{
+    public void  calculate(int a,int b){
+        System.out.println("your result is:" + a+b);
+    }
+
+}
+
+public class pracrice_set_12 {
+    public static void main(String[] args) {
+        System.out.println("I am main method");
+    }
+
+    
+}
