cdf1fe9fca390b74bb8bfc5a223da78524b9d76f
ravina1606 <160000675+ravina1606@users.noreply.github.com>
2025-03-17 23:06:04 +0530

    Add files via upload

---
+++ problem12_4.java
@@ -0,0 +1,22 @@
+
+
+import folder.folder1.folder2.problem12_2;
+
+class c1 extends problem12_2{
+    void problem2(){
+        System.out.println(proInt);
+        System.out.println(defInt);
+    }
+}
+
+
+
+public class problem12_4{
+        public static void main(String[] args) {
+        System.out.println("I am main method");
+        c1 c=new c1();
+    }
+
+    
+}
+
