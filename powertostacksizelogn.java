e59fca048b6145120b951fd1b0dbbb54118e4991
ravina1606 <160000675+ravina1606@users.noreply.github.com>
2025-04-22 22:08:47 +0530

    Add files via upload

---
+++ powertostacksizelogn.java
@@ -0,0 +1,28 @@
+public class powertostacksizelogn {
+   
+        public static int calcpower(int x,int n){
+            if(n==0){
+                return 1;
+            }
+            if(x==1)
+            {
+                return 0;
+    
+            }
+           if(n%2==0)
+           {
+            return calcpower(x, n/2)*calcpower(x, n/2);
+           }
+           else{
+            return calcpower(x, n/2)*calcpower(x, n/2)*x;
+           }
+    
+        }
+        public static void main(String[] args) {
+            int x=2,n=5;
+            int ans=calcpower(x, n);
+            System.out.println(ans);
+        }
+        
+    
+}
