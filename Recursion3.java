991b35c0e22ae717e7ce3daa8f2d857c70efaf74
ravina1606 <160000675+ravina1606@users.noreply.github.com>
2025-04-25 23:01:05 +0530

    Add files via upload

---
+++ Recursion3.java
@@ -0,0 +1,31 @@
+public class Recursion3 {
+    public static int first=-1;
+    public static int last=-1;
+
+    public static void findoccurence(String str,int idx,char element){
+   if(idx==str.length()){
+    System.out.println(first);
+    System.err.println(last);
+    return;
+
+   }
+        char currChar=str.charAt(idx);
+      if(currChar==element)
+      {
+        if(first==-1)
+        {
+            first=idx;
+        }
+        else{
+            last=idx;
+        }
+      }
+      findoccurence(str, idx+1, element);
+    }
+    public static void main(String[] args) {
+        String str="abaacdaefaah";
+        //time complexity O(n)
+        findoccurence(str, 0, 'a');
+    }
+    
+}
