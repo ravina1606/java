cc6d602b9482c82f07797ce066154b2fafa1ce96
ravina1606 <160000675+ravina1606@users.noreply.github.com>
2025-04-28 23:32:45 +0530

    Add files via upload

---
+++ Recursion6.java
@@ -0,0 +1,24 @@
+public class Recursion6 {
+
+    public static boolean[]map=new boolean[26];
+    public static void removeDuplicatea(String str,int idx ,String newString){
+if(idx==str.length()){
+    System.out.println(newString);
+    return;
+}
+        char currchar=str.charAt(idx);
+        if(map[currchar -'a']==true){
+            removeDuplicatea(str, idx+1, newString);
+        }
+        else{
+        newString+=currchar;
+        map[currchar-'a']=true;
+        removeDuplicatea(str, idx+1, newString);
+        }
+    }
+    public static void main(String[] args) {
+        String str="abbccda";
+        //time complexity=O(n)
+        removeDuplicatea(str, 0, "");
+    }
+}
