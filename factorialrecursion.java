84badd76254257fa7888ad59316d3a4734e2d88a
ravina1606 <160000675+ravina1606@users.noreply.github.com>
2025-05-08 22:17:49 +0530

    Add files via upload

---
+++ factorialrecursion.java
@@ -0,0 +1,16 @@
+public class factorialrecursion {
+    public static int calcfactorial(int n){
+        if(n==1||n==0)
+        {
+            return 1;
+        }
+      int factnm1  = calcfactorial(n-1);
+      int fact_n=n*factnm1;
+      return fact_n
+    }
+    public static void main(String[] args) {
+        int n=5;
+        int ans=calcfactorial(n);
+        System.out.println(ans);
+    }
+}
